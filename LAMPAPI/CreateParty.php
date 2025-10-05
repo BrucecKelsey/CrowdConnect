5f34bd760622ed290bba6404b4b14471f2119c81
truechip <truechip122@gmail.com>
2025-10-05 16:34:11 -0400

    Change

---
+++ LAMPAPI/CreateParty.php
@@ -0,0 +1,48 @@
+<?php
+// LAMPAPI/CreateParty.php
+$inData = getRequestInfo();
+$partyName = $inData["partyName"];
+$djId = $inData["djId"];
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    $stmt = $conn->prepare("INSERT INTO Parties (PartyName, DJId) VALUES (?, ?)");
+    $stmt->bind_param("si", $partyName, $djId);
+    if ($stmt->execute())
+    {
+        $partyId = $conn->insert_id;
+        returnWithInfo($partyId, $partyName);
+    }
+    else
+    {
+        returnWithError($stmt->error);
+    }
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+function returnWithError($err)
+{
+    $retValue = '{"partyId":0,"partyName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+function returnWithInfo($partyId, $partyName)
+{
+    $retValue = '{"partyId":' . $partyId . ',"partyName":"' . $partyName . '","error":""}';
+    sendResultInfoAsJson($retValue);
+}
+?>
