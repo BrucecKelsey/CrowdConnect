a20a579b0f9aa10d4d5a684093844e1424d8fada
Bruce Kelsey <truechip122@gmail.com>
2025-11-02 15:08:17 -0500

    quick

--- LAMPAPI/stripe-php-simple.php
+++ LAMPAPI/stripe-php-simple.php
@@ -98,7 +98,11 @@ class SimpleStripe {
         $data = [
             'amount' => $amount,
             'currency' => $currency,
-            'automatic_payment_methods[enabled]' => 'true'
+            'automatic_payment_methods[enabled]' => 'true',
+            'automatic_payment_methods[allow_redirects]' => 'always',
+            'payment_method_types[]' => 'card',
+            'payment_method_types[]' => 'apple_pay',
+            'payment_method_types[]' => 'google_pay'
         ];
         
         // Add metadata with proper Stripe format
